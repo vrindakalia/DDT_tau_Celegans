9392927d758bbbb76089bfe05e557a27f9e7f3d0
Vrinda Kalia <vrindakalia3@gmail.com>
2021-02-15 12:38:23 -0500

    worm size

---
+++ worm_size/README.md
@@ -0,0 +1,13 @@
+# Worm size
+
+The COPAS Flow Pilot (FP) 250 is a tool used for high-throughput manipulation of *C. elegans*. For each worm that passes through the flow cell, the COPAS FP-250 determines its time-of-flight (TOF), which represents the length of the worm passing through the flow cell, and the extinction, which represents the optical density or thickness of the worm passing through the flow cell. After hypochlorite synchronization, eggs were allowed to hatch and develop on 10cm NGM plates with 3uM DDT and the solvent control. Worms were sorted through the COPAS FP 250 to measure TOF and extinction 46-50 hours after synchronization (around the L4 stage) and at 70-72 hours post synchronization (around young adults). Measures of TOF and extinction were compared across the strains and treatment groups and the two time points. 
+
+
+Below, we see the flow cell and the movement of worms through th flow cell and their size and thickness is determined through laser excitation.
+<img src = "https://github.com/vrindakalia/DDT_tau_Celegans/blob/main/worm_size/figures/flow_cell_wm.gif">
+
+Data for each object can be scatte plot to determine a certain population to be gated.
+
+<img src = "https://github.com/vrindakalia/DDT_tau_Celegans/blob/main/worm_size/figures/copas_run.gif">
+
+
---
+++ worm_size/code/worm_size.R
@@ -0,0 +1,141 @@
+############################################################
+# ANALYZE WORM SIZE DATA
+############################################################
+
+library(tidyverse)
+library(ggpubr)
+library(gridExtra)
+library(cowplot)
+# CALL IN L4 DATA
+
+all_l4s <- read_tsv("worm_size/data/L4s_bars.txt") %>% 
+    mutate(time = "46-to-50")
+
+all_adults <- read_tsv("worm_size/data/adults_bars_size.txt") %>% 
+    mutate(time = "70-to-72")
+
+all_ever <- rbind(all_l4s, all_adults)
+
+all_ever$trt <- factor(all_ever$trt, levels = c("DMSO", "DDT"))
+all_ever$strain <- factor(all_ever$strain, levels = c("N2", "BR5271", "BR5270"))
+
+l4s <- all_ever %>% 
+    filter(time == "46-to-50") %>% 
+    ggplot(aes(x = strain, y = mean_ext, fill = trt)) +
+    geom_errorbar(aes(ymin = mean_ext - sd_ext, ymax = mean_ext + sd_ext),  position = position_dodge(width = 0.58), width = 0.2) +
+    geom_bar(stat = "identity", position = "dodge", width = 0.6) +
+    scale_fill_manual(values = c( "#50BFC3", "#214D72")) +
+    theme_classic() +
+    #theme(legend.position = c(0.94,0.98), 
+    # axis.text.x = element_text(size = 9)) +
+    labs(x = "",
+         y = "Extinction", fill= "") +
+    guides(shape = guide_legend(override.aes = list(size = 0.7))) +
+    guides(fill = guide_legend(override.aes = list(size = 0.5))) +
+    theme(legend.text = element_text(size = 7), axis.text.x = element_text(size = 7),
+          plot.title = element_text(size = 10)) +
+    geom_segment(aes(x=1.85, xend=2.25, y=1150, yend=1150)) +
+    annotate("text", x = 2.05, y = 1175, label = "***", size = 4) +
+    geom_segment(aes(x=0.85, xend=1.25, y=1080, yend=1080)) +
+    annotate("text", x = 1.05, y = 1105, label = "***", size = 4) +
+    geom_segment(aes(x=1.85, xend=2.85, y = 1300, yend = 1300)) +
+    geom_segment(aes(x=1.85, xend=1.85, y = 1250, yend=1300)) +
+    geom_segment(aes(x=2.85, xend=2.85, y=1250, yend=1300)) +
+    annotate("text", x = 2.35, y = 1330, label = "***", size = 4) +
+    geom_segment(aes(x=2.85, xend=3.25, y=875, yend=875)) +
+    annotate("text", x = 3.05, y = 900, label = "***", size = 4) +
+    scale_x_discrete(labels = c("N2 \n(Control)", "BR5271 \n(Non-agg)", "BR5270 \n(Agg)")) +
+    ggtitle("46-50 hours: L4 stage")
+
+adults <- all_ever %>% 
+    filter(time == "70-to-72") %>% 
+    ggplot(aes(x = strain, y = mean_ext, fill = trt)) +
+    geom_errorbar(aes(ymin = mean_ext - sd_ext, ymax = mean_ext + sd_ext),  position = position_dodge(width = 0.58), width = 0.2) +
+    geom_bar(stat = "identity", position = "dodge", width = 0.6) +
+    scale_fill_manual(values = c( "#50BFC3", "#214D72")) +
+    theme_classic() +
+    #theme(legend.position = c(0.94,0.98), 
+    # axis.text.x = element_text(size = 9)) +
+    labs(x = "",
+         y = "Extinction", fill= "") +
+    guides(shape = guide_legend(override.aes = list(size = 0.7))) +
+    guides(fill = guide_legend(override.aes = list(size = 0.5))) +
+    theme(legend.text = element_text(size = 7), axis.text.x = element_text(size = 7),
+          plot.title = element_text(size = 10)) +
+    geom_segment(aes(x=0.85, xend=3.25, y=2100, yend=2100)) +
+    annotate("text", x = 2.05, y = 2125, label = "***", size = 4) +
+    scale_x_discrete(labels = c("N2 \n(Control)", "BR5271 \n(Non-agg)", "BR5270 \n(Agg)")) +
+    ggtitle("70-72 hours: adult stage")
+
+l4s.tof <- all_ever %>% 
+    filter(time == "46-to-50") %>% 
+    ggplot(aes(x = strain, y = mean_tof, fill = trt)) +
+    geom_errorbar(aes(ymin = mean_tof - sd_tof, ymax = mean_tof + sd_tof),  position = position_dodge(width = 0.7), width = 0.15) +
+    geom_bar(stat = "identity", position = "dodge", width = 0.7) +
+    scale_fill_manual(values = c( "#50BFC3", "#214D72")) +
+    theme_classic() +
+    #theme(legend.position = c(0.94,0.98), 
+    # axis.text.x = element_text(size = 9)) +
+    labs(x = "",
+         y = "Time of flight (TOF)", fill= "") +
+    guides(shape = guide_legend(override.aes = list(size = 0.7))) +
+    guides(fill = guide_legend(override.aes = list(size = 0.5))) +
+    theme(legend.text = element_text(size = 7), axis.text.x = element_text(angle = 30, hjust=1,size = 7),
+          plot.title = element_text(size = 10)) +
+    geom_segment(aes(x=1.85, xend=2.25, y=1680, yend=1680)) +
+    annotate("text", x = 2.05, y = 1700, label = "***", size = 4) +
+    geom_segment(aes(x=0.85, xend=1.25, y=1680, yend=1680)) +
+    annotate("text", x = 1.05, y = 1700, label = "***", size = 4) +
+    geom_segment(aes(x=1.85, xend=2.85, y = 1900, yend = 1900)) +
+    geom_segment(aes(x=1.85, xend=1.85, y = 1850, yend=1900)) +
+    geom_segment(aes(x=2.85, xend=2.85, y=1850, yend=1900)) +
+    annotate("text", x = 2.35, y = 1930, label = "***", size = 4) +
+    geom_segment(aes(x=2.85, xend=3.25, y=1450, yend=1450)) +
+    annotate("text", x = 3.05, y = 1480, label = "***", size = 4) +
+    scale_x_discrete(labels = c("N2 (Control)", "BR5271 (Non-agg)", "BR5270 (Agg)")) +
+    ggtitle("46-50 hours") +
+    ylim(0,2590)
+
+adults.tof <- all_ever %>% 
+    filter(time == "70-to-72") %>% 
+    ggplot(aes(x = strain, y = mean_tof, fill = trt)) +
+    geom_errorbar(aes(ymin = mean_tof - sd_tof, ymax = mean_tof + sd_tof),  position = position_dodge(width = 0.7), width = 0.15) +
+    geom_bar(stat = "identity", position = "dodge", width = 0.7) +
+    scale_fill_manual(values = c( "#50BFC3", "#214D72")) +
+    theme_classic() +
+    #theme(legend.position = c(0.94,0.98), 
+    # axis.text.x = element_text(size = 9)) +
+    labs(x = "",
+         y = "Time of flight (TOF)", fill= "") +
+    ggtitle("70-72 hours") +
+    guides(shape = guide_legend(override.aes = list(size = 0.7))) +
+    guides(fill = guide_legend(override.aes = list(size = 0.5))) +
+    theme(legend.text = element_text(size = 7), axis.text.x = element_text(angle = 30, hjust=1, size = 7),
+          plot.title = element_text(size = 10)) +
+    geom_segment(aes(x=0.85, xend=3.25, y=2500, yend=2500)) +
+    annotate("text", x = 2.05, y = 2525, label = "***", size = 4) +
+    scale_x_discrete(labels = c("N2 (Control)", "BR5271 (Non-agg)", "BR5270 (Agg)")) +
+    ylim(0,2590)
+
+prow <- plot_grid(
+    l4s + theme(legend.position="none"),
+    adults + theme(legend.position="none"),
+    align = 'vh',
+    labels = c("C", "D"),
+    hjust = -1,
+    nrow =2
+)
+
+legend <- get_legend(
+    # create some space to the left of the legend
+    l4s +
+        guides(color = guide_legend(nrow = 1)) +
+        theme(legend.position = "bottom"))
+
+
+plot_grid(prow, legend, ncol = 1, rel_heights = c(1, .1))
+
+# To save plot as a pdf
+pdf("worm_size/figures/extinction_l4_adult.pdf", width = 3, height = 6)
+plot_grid(prow, legend, ncol = 1, rel_heights = c(1, .1))
+dev.off()
